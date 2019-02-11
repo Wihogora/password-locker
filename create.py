ea57810148e9f318cbfbec34a60feabe04d06646
Wihogora Wivine <wwihogora@gmail.com>
2019-02-11 17:58:28 +0200

    .

--- create.py
+++ create.py
@@ -1,35 +1,18 @@
-import unittest # Importing the unittest module
 
-class create:
+
+class Create:
     """
     Class that generates new instances of create
 
     """
-def __init__(self,first_name,last_name,email,username,password):
+    create_list = []
+    def __init__(self,first_name,last_name,email,username,password):
 
-    self.first_name = first_name
-    self.last_name = last_name
-    self.email = email
-    self.username = username
-    self.password = password
-
-def setUp(self):
-    '''
-    Set up method to run before each test cases.
-    '''
-    self.new_create = create("wivine","wihogora","0712345678","wivi","wivi@gmail.com","wivi") # create contact object
-def test_init(self):
-    '''
-    test_init test case to test if the object is initialized properly
-    '''
-    self.assertEqual(self.new_create.first_name,"wivine")
-    self.assertEqual(self.new_create.last_name,"wihogora")
-    self.assertEqual(self.new_create.phone_number,"0712345678")
-    self.assertEqual(self.new_create.email,"wivi")
-    self.assertEqual(self.new_create.email,"wivi@gmail.com")
-    self.assertEqual(self.new_create.email,"wivi")
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.username = username
+        self.password = password
 
 
-if __name__ == '__main__':
-    unittest.main()
 pass
---
+++ user_test.py
@@ -0,0 +1,28 @@
+import unittest
+from create import Create
+def setUp(self):
+    '''
+    Set up method to run before each test cases.
+    '''
+    self.new_create = Create("wivine","wihogora","wivi@gmail.com","wivi","wivi") # create contact object
+def test_init(self):
+    '''
+    test_init test case to test if the object is initialized properly
+    '''
+    self.assertEqual(self.new_create.first_name,"wivine")
+    self.assertEqual(self.new_create.last_name,"wihogora")
+    self.assertEqual(self.new_create.email,"wivi@gmail.com")
+    self.assertEqual(self.new_create.username,"wivi")
+    self.assertEqual(self.new_create.password,"wivi")
+
+def test_save_create(self):
+    '''
+    test_save_contact test case to test if the contact object is saved into
+    the contact list
+    '''
+    self.new_create.save_create() # saving the new contact
+    self.assertEqual(len(Create.create_list),1)
+
+
+if __name__ == '__main__':
+    unittest.main()
