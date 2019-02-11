4db687a3b221d4d9323ed1b4f0826df4ed345cff
Wihogora Wivine <wwihogora@gmail.com>
2019-02-11 15:51:59 +0200

    Add instance

---
+++ create.py
@@ -0,0 +1,15 @@
+class create:
+    """
+    Class that generates new instances of create
+
+    """
+def __init__(self,first_name,last_name,email,username,password):
+
+     
+
+    self.first_name = first_name
+    self.last_name = last_name
+    self.email = email
+    self.username = username
+    self.password = password
+pass
