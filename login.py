1f27f79f7bc0a8fb96b5d6788d478804b2ce3fab
Wihogora Wivine <wwihogora@gmail.com>
2019-02-12 13:52:39 +0200

    Add find user in login.py

--- login.py
+++ login.py
@@ -1,10 +1,59 @@
 from user import *
-def create_user(irst_name,last_name,email,username,password):
-    '''
-    Function to create a new contact
-    '''
-    new_contact = Contact(fname,lname,phone,email)
-    return new_contact
+    def create_user(first_name,last_name,email,username,password):
+        '''
+        Function to create a new user
+        '''
+        new_user = User(first_name,last_name,email,username,password)
+        return new_user
+    
+    def save_user(user):
+        '''
+        Function to save user
+        '''
+        user.save_user()
+
+    def del_user(user):
+        '''
+        Function to delete a user
+        '''
+        user.delete_user()
+
+    def find_user(username):
+        '''
+        Function that finds a user by username and returns the user
+        '''
+        return User.find_by_username(username)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Username=input()
 # Email=input()
