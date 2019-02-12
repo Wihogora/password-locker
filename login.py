a1ba4953963da7473640172fc36e03d9509c0c51
Wihogora Wivine <wwihogora@gmail.com>
2019-02-12 13:37:56 +0200

    Add create user in login.py

--- login.py
+++ login.py
@@ -1,11 +1,19 @@
-Username=input()
-Email=input()
-Password=input()
-PhoneNumber=input()
-print(Username,Email,Password,PhoneNumber)
-e=input()
-f=input()
-if(Email==e and Password==f):
-    print("Welcome" +Username)
-else:
-    print("Failed to login ")  
+from user import *
+def create_user(irst_name,last_name,email,username,password):
+    '''
+    Function to create a new contact
+    '''
+    new_contact = Contact(fname,lname,phone,email)
+    return new_contact
+
+# Username=input()
+# Email=input()
+# Password=input()
+# PhoneNumber=input()
+# print(Username,Email,Password,PhoneNumber)
+# e=input()
+# f=input()
+# if(Email==e and Password==f):
+#     print("Welcome" +Username)
+# else:
+#     print("Failed to login ")  
